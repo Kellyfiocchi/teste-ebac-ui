567d872d772f28f2298960cec1b3199bb8e04ad5
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-09-13 17:39:45 -0300

    novas atualizações

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress"/>
+var faker = require('faker');
+const Faker = require('faker/lib');
+
+describe('Funcionalidade Pré Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Deve completar o pré cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste@teste.com')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('.form-row-last > label').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+        
+    });
+
+    
+});
