c5c4e3ac459a2e2e4ac7aee49b622963eecd167c
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-09-14 13:33:35 -0300

    alterações nos testes automatizados

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -9,13 +9,18 @@ describe('Funcionalidade Pré Cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+    
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@teste.com')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('.form-row-last > label').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('.form-row-last > label').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,32 @@
+///  <reference types="cypress"/>
+
+describe('Funcionalidade Pagina de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um  produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Argus All-Weather Tank')
+            .click()
+
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+        cy.get('[class="product-block grid"]')
+            .contains('Argus All-Weather Tank').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain',quantidade + ' × “Argus All-Weather Tank” foram adicionados no seu carrinho.')
+
+    });
+});
