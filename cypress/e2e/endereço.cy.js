d71b5de7a9e4ac3e966d73a1b0b8d98fc533e085
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-09-21 17:57:17 -0300

    massa de dados

--- cypress/e2e/endereço.cy.js
+++ cypress/e2e/endereço.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,8 +11,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('João','Silva','Ebac','Brasil','Rua Para','219','Ribeirao Pires','Sao Paulo','09430-350','11992992854','teste@teste.com')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso -- Usando arquivos de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    
 });
