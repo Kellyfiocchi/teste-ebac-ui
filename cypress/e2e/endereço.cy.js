ca67efd56ae80c532b6daecaf179e7fe193b9837
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-09-21 16:12:03 -0300

    update page-objects

--- cypress/e2e/endereço.cy.js
+++ cypress/e2e/endereço.cy.js
@@ -1,16 +1,17 @@
 /// <reference types="cypress"/>
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
         
     });
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-       
-        //cadastro de endereço
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('João','Silva','Ebac','Brasil','Rua Para','219','Ribeirao Pires','Sao Paulo','09430-350','11992992854','teste@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
---
+++ cypress/support/page-objects/endereco.page.js
@@ -0,0 +1,28 @@
+class EndereçoPage {
+
+    editarEnderecoFaturamento(nome, sobrenome, empresa, pais, endereco, numero, cidade, estado, cep, telefone, email) {
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+        cy.get(':nth-child(1) > .title > .edit').click()
+
+        cy.get('#billing_first_name').clear().type(nome)
+        cy.get('#billing_last_name').clear().type(sobrenome)
+        cy.get('#billing_company').clear().type(empresa)
+
+        cy.get('#select2-billing_country-container').click().type(pais).get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').clear().type(endereco)
+        cy.get('#billing_address_2').clear().type(numero)
+        cy.get('#billing_city').clear().type(cidade)
+        cy.get('#select2-billing_state-container').click().type(estado +'{enter}')
+        cy.get('#billing_postcode').clear().type(cep)
+        cy.get('#billing_phone').clear().type(telefone)
+        cy.get('#billing_email').clear().type(email)
+        cy.get('.button').click()
+    }
+
+    editarEnderecoEntrega() {
+        //elementos + ações
+    }
+
+}
+
+export default new EndereçoPage()
